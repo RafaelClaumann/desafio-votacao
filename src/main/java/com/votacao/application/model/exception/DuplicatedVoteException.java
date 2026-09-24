0744080164aa7b0c2f827764f4eea7feb05732c2
Rafael Claumann <rafael_claumann@hotmail.com>
2026-09-24 16:33:07 -0300

    i18n: localize application exceptions to Portuguese
    - Translate all exception messages from English to Portuguese
    - Update DuplicatedVoteException, PautaNotFoundException, SessaoIsClosedException, SessaoIsOpenException, and SessaoNotFoundException
    - Ensure consistent error messaging across the application

--- src/main/java/com/votacao/application/model/exception/DuplicatedVoteException.java
+++ src/main/java/com/votacao/application/model/exception/DuplicatedVoteException.java
@@ -3,7 +3,7 @@ package com.votacao.application.model.exception;
 public class DuplicatedVoteException extends RuntimeException {
 
     public DuplicatedVoteException(Long idSessao, String documento) {
-        super("Duplicated vote for document: " + documento + " in session with id: " + idSessao);
+        super("O documento " + documento + " já votou na sessão " + idSessao);
     }
 
 }
--- src/main/java/com/votacao/application/model/exception/SessaoIsClosedException.java
+++ src/main/java/com/votacao/application/model/exception/SessaoIsClosedException.java
@@ -3,7 +3,7 @@ package com.votacao.application.model.exception;
 public class SessaoIsClosedException extends RuntimeException {
 
     public SessaoIsClosedException(Long id) {
-        super("Sessão with id: " + id + " is closed");
+        super("A sessão " + id + " está fechada");
     }
 
 }
